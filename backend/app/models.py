8c768679106b2079092ff76c734a240ed136fd01
Ryan Boll <bollr38@hotmail.com>
2025-06-21 16:29:00 -0500

    added Flashcard and Vaultcar

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,5 @@
+# setting up models to create tables in database
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
@@ -8,4 +10,29 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 
+# TODO: Need foreign keys to tie data. Add tables to ERD and complete flow
 
+class Flashcard(Base):
+    __tablename__="flashcard"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    card_id: Mapped[str] = mapped_column(db.String(350), unique=True, nullable=False)
+    question: Mapped[str] = mapped_column(db.String(350), unique=True, nullable=False)
+    answer: Mapped[str] = mapped_column(db.String(350), nullable=False)
+    tags: Mapped[str] = mapped_column(db.String(150), nullable=False)
+    emotion: Mapped[str] = mapped_column(db.String(150))
+    narrative_type: Mapped[str] = mapped_column(db.String(150), nullable=False)
+    usage_mode: Mapped[str] = mapped_column(db.String(150), nullable=False)
+
+class Vaultcard(Base):
+    __tablename__="vaultcard"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    card_id: Mapped[str] = mapped_column(db.String(350), unique=True, nullable=False)
+    headline: Mapped[str] = mapped_column(db.String(350), nullable=False)
+    body: Mapped[str] = mapped_column(db.String(350), nullable=False)
+    prompt: Mapped[str] = mapped_column(db.String(150), nullable=False)
+    tags: Mapped[str] = mapped_column(db.String(150), nullable=False)
+    emotion: Mapped[str] = mapped_column(db.String(150))
+    narrative_type: Mapped[str] = mapped_column(db.String(150), nullable=False)
+    usage_mode: Mapped[str] = mapped_column(db.String(150), nullable=False)
