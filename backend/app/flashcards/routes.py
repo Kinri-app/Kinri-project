1712fd20080e8dffcc4c8135f4c504686390297f
Ryan Boll <bollr38@hotmail.com>
2025-07-12 15:19:30 -0500

    set up preliminary flashcard get route. Need table change when ready

--- backend/app/flashcards/routes.py
+++ backend/app/flashcards/routes.py
@@ -1,30 +1,21 @@
-from app.flashcards import flashcards_bp
-from app.flashcards.schemas import flashcard_schema, flashcards_schema
-from flask import request, jsonify
-from app.models import Flashcard, db
+from flask import request, jsonify, Blueprint
 from sqlalchemy import select
-from marshmallow import ValidationError
+from app import supabase
+
+flashcards_bp = Blueprint('flashcards_bp',__name__)
 
 # --------------Flashcard routes------------------------------
 
 # Function to return all flashcards
 @flashcards_bp.route("/", methods=["GET"])
 def get_flashcards():
-    query = select(Flashcard)
-    result = db.session.execute(query).scalars().all()
-
-    return flashcards_schema.jsonify(result), 200
-
-# Function to return single flashcard by it's ID
-@flashcards_bp("/<int:flashcard_id")
-def get_flashcard_by_id(flashcard_id):
     try:
-        query = select(Flashcard).where(Flashcard.id == flashcard_id)
-        single_flashcard = db.session.execute(query)
+        response = supabase.table("tier1_2_batch1").select("*").execute()
 
-    except ValidationError as e:
-        return jsonify(e.messages), 400
-    
-    db.session.commit()
-    
-    return jsonify(single_flashcard)
+        if not response.data:
+            return jsonify({"error": "Failed to retrieve supabase data."}), 404
+        
+        return jsonify(response.data)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
